591a777ed9439ba2d08621cd0c9fa1d00ae63a01
nicobrch <nicolas.brch@gmail.com>
2025-12-10 12:49:20 -0300

    enable apis

--- main.tf
+++ main.tf
@@ -3,12 +3,43 @@ provider "google" {
   region  = var.region
 }
 
+# -- APIs --
+
 # Enable the Firestore API automatically
 resource "google_project_service" "firestore" {
   project            = var.project_id
   service            = "firestore.googleapis.com"
   disable_on_destroy = false
 }
+
+# Enable Secret Manager API
+resource "google_project_service" "secret_manager" {
+  project            = var.project_id
+  service            = "secretmanager.googleapis.com"
+  disable_on_destroy = false
+}
+
+# Enable Cloud Run & Cloud Build APIs
+resource "google_project_service" "cloud_run" {
+  project            = var.project_id
+  service            = "run.googleapis.com"
+  disable_on_destroy = false
+}
+
+resource "google_project_service" "cloud_build" {
+  project            = var.project_id
+  service            = "cloudbuild.googleapis.com"
+  disable_on_destroy = false
+}
+
+# Enable Artifact Registry API
+resource "google_project_service" "artifact_registry" {
+  project            = var.project_id
+  service            = "artifactregistry.googleapis.com"
+  disable_on_destroy = false
+}
+
+# -- Services --
 
 # Create the Firestore Native Database
 resource "google_firestore_database" "database" {
