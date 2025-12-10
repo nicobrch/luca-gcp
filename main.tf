12b601d634ff97e5e24f91763cee56c536a7cf47
nicobrch <nicolas.brch@gmail.com>
2025-12-10 14:30:13 -0300

    add luca services

--- main.tf
+++ main.tf
@@ -59,3 +59,41 @@ resource "google_firestore_database" "database" {
 
   depends_on = [google_project_service.firestore]
 }
+
+# Create the Luca's Artifact Registry Repository
+resource "google_artifact_registry_repository" "repository" {
+  provider     = google
+  location     = var.region
+  repository_id = var.luca_artifact_registry_name
+  description  = "Artifact Registry for Cloud Run service images"
+  format       = "DOCKER"
+  mode         = "STANDARD"
+
+  depends_on = [google_project_service.artifact_registry]
+}
+
+# Create a Secret Manager secret to store sensitive configuration
+resource "google_secret_manager_secret" "luca_config_secret" {
+  secret_id = var.luca_secret_name
+  replication {
+    auto {}
+  }
+  depends_on = [google_project_service.secret_manager]
+}
+
+# Luca's Github Actions Service Account
+resource "google_service_account" "github_actions_sa" {
+  account_id   = var.luca_github_actions_sa_name
+  display_name = "Luca Github Actions Service Account"
+
+  depends_on = [google_project_service.iam]
+}
+
+# Grant IAM roles to the Service Account
+resource "google_project_iam_member" "github_actions_sa_roles" {
+  for_each = toset(var.luca_github_actions_sa_roles)
+
+  project = var.project_id
+  role    = each.value
+  member  = "serviceAccount:${google_service_account.github_actions_sa.email}"
+}
--- variables.tf
+++ variables.tf
@@ -8,3 +8,33 @@ variable "region" {
   type        = string
   default     = "us-east4"
 }
+
+variable "luca_artifact_registry_name" {
+  description = "The name of the Artifact Registry repository"
+  type        = string
+  default     = "luca-artifact-repo"
+}
+
+variable "luca_secret_name" {
+  description = "The name of the Secret Manager secret for Luca"
+  type        = string
+  default     = "luca-secret"
+}
+
+variable "luca_github_actions_sa_name" {
+  description = "The name of the Service Account for GitHub actionss"
+  type        = string
+  default     = "luca-github-actions-sa"
+}
+
+variable "luca_github_actions_sa_roles" {
+  description = "The roles to assign to the GitHub Actions Service Account"
+  type        = list(string)
+  default = [
+    "roles/run.admin",
+    "roles/iam.serviceAccountUser",
+    "roles/artifactregistry.writer",
+    "roles/secretmanager.secretAccessor",
+    "roles/secretmanager.secretVersionAdder",
+  ]
+}
